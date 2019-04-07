a492af3cd658ae172121f3c41a7f5caf84b3a4eb
Alexandru Ionascu <alexandruionascu.bd@gmail.com>
2019-04-07 15:46:58 +0300

    added first emun-ro CRA build

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-1_B3kBQTaHf',
+        userPoolClientId: '14qdip6ng8514j37m34bc2f5gg',
+        region: 'eu-west-1'
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
---
+++ precache-manifest.90d5dc78b5408e675995f2c7117eb5d9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b7c2c08efaaa29d967ca6a26107e802",
+    "url": "/static/media/logo_3.3b7c2c08.png"
+  },
+  {
+    "revision": "2456f72e9860005eb2bb",
+    "url": "/static/css/main.17d90c85.chunk.css"
+  },
+  {
+    "revision": "fd0b11df5c924bacc0d8439ff7cb1911",
+    "url": "/static/media/editor.fd0b11df.png"
+  },
+  {
+    "revision": "306fb869fc55de6c786e",
+    "url": "/static/js/1.306fb869.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2456f72e9860005eb2bb",
+    "url": "/static/js/main.2456f72e.chunk.js"
+  },
+  {
+    "revision": "fb5ef752f16991c120d079b2a2f5856c",
+    "url": "/static/media/teen_coding_illustration.fb5ef752.svg"
+  },
+  {
+    "revision": "f9bed15856252f0c7fbb2fdb3ab5c2d7",
+    "url": "/static/media/coding_illustration.f9bed158.svg"
+  },
+  {
+    "revision": "395de01aabf03ad93eccfe91c1ba2478",
+    "url": "/static/media/devices_illustration.395de01a.svg"
+  },
+  {
+    "revision": "306fb869fc55de6c786e",
+    "url": "/static/css/1.a6fa30e5.chunk.css"
+  },
+  {
+    "revision": "88706a6d9bcaaefad55e254ed920afd2",
+    "url": "/index.html"
+  }
+];
